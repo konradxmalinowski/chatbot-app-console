20b1efeeb233d729d6455bccabe4c91f54cbfbe5
Konrad <malinowski.konrad45@gmail.com>
2026-06-06 08:19:15 +0200

    style: apply Ruff formatting to main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnableConfig
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.chat_history import InMemoryChatMessageHistory
@@ -13,27 +13,28 @@ load_dotenv(verbose=True)
 gemini_api_key = os.environ.get("GEMINI_API_KEY")
 gemini_llm_model = os.environ.get("GEMINI_LLM_MODEL")
 
-llm = ChatGoogleGenerativeAI(
-    model=gemini_llm_model,
-    google_api_key=gemini_api_key
-)
+llm = ChatGoogleGenerativeAI(model=gemini_llm_model, google_api_key=gemini_api_key)
 
 parser = StrOutputParser()
 
-chat_template_prompt = ChatPromptTemplate([
-    ("system", SYSTEM_PROMPT),
-    MessagesPlaceholder(variable_name="chat_history"),
-    ("human", "{input}"),
-])
+chat_template_prompt = ChatPromptTemplate(
+    [
+        ("system", SYSTEM_PROMPT),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{input}"),
+    ]
+)
 
 base_chain = chat_template_prompt | llm | parser
 
 history_state = {}
 
+
 def get_session_history(session_id: str):
     if session_id not in history_state:
         history_state[session_id] = InMemoryChatMessageHistory()
     return history_state[session_id]
+
 
 conversation_chain = RunnableWithMessageHistory(
     runnable=base_chain,
@@ -42,15 +43,14 @@ conversation_chain = RunnableWithMessageHistory(
     history_messages_key="chat_history",
 )
 
+
 def chat_with_llm(user_prompt: str):
     config = RunnableConfig({"configurable": {"session_id": "1"}})
 
-    response = conversation_chain.invoke(
-        {"input": user_prompt},
-        config=config
-    )
+    response = conversation_chain.invoke({"input": user_prompt}, config=config)
 
     print(f"AI: {response}")
+
 
 def main():
     print("Welcome! We introduce chatbot-app v.0!")
@@ -65,6 +65,7 @@ def main():
             chat_with_llm(user_prompt)
         except Exception as e:
             print(f"Error: {e}")
+
 
 if __name__ == "__main__":
     main()
